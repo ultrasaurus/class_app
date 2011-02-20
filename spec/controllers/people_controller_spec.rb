1256699ea2eb35abbe694f2dbc705551d4671f7d
Sarah Allen <sarah@ultrasaurus.com>
2011-02-20 13:35:01 -0800

    people controller spec

---
+++ spec/controllers/people_controller_spec.rb
@@ -0,0 +1,23 @@
+# START:one
+# START:two
+require 'spec_helper'
+
+describe PeopleController do
+  describe "GET index" do
+    it "renders" do
+      get :index        #<label id="code.get_index"/>
+      response.should render_template(:index) #<label id="code.should_render"/>
+    end
+# END:one     
+    it "renders with text param" do
+      sample_text = "something"      #<label id="code.local.sample_text"/>
+      get :index, {:text => sample_text}    #<label id="code.get.text.param"/>
+      assigns[:message].should == sample_text   #<label id="code.assigs.message"/>
+      response.should render_template(:index)
+    end
+# START:one     
+  end
+end
+# END:one     
+
+
