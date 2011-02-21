c802ee98901198e2d3a048860536457806885f33
Sarah Allen <sarah@ultrasaurus.com>
2011-02-20 20:53:32 -0800

    test for 200 success as first example

--- spec/requests/homepage_spec.rb
+++ spec/requests/homepage_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe "the homepage" do
   it "says welcome" do
     visit '/'  #<label id="code.root_path"/> 
-    page.should have_content("Welcome to class")  #<label id="code.page_should_welcome"/>
+    page.status_code.should == 200   #<label id="code.page_200_status"/>
   end
 end
