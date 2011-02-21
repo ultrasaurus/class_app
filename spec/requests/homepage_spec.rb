fa13dbfcc6cb6478e2f3d8fa793e3d98e01a0a48
Sarah Allen <sarah@ultrasaurus.com>
2011-02-21 05:41:33 -0800

    fixed name of spec

--- spec/requests/homepage_spec.rb
+++ spec/requests/homepage_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "the homepage" do
-  it "says welcome" do
+  it "exists" do
     visit '/'  #<label id="code.root_path"/> 
     page.status_code.should == 200   #<label id="code.page_200_status"/>
   end
