2c0e2c27d7fbe12461fedfd5da898f29c952724d
Sarah Allen <sarah@ultrasaurus.com>
2011-02-27 14:56:22 -0800

    example for @people

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
 
 describe PeopleController do
+#START:create_people
+  before do
+    @emmy = Person.create! :first_name => "Emmy", :last_name => "Noether"
+    @marie = Person.create! :first_name => "Marie", :last_name => "Curie"
+    @lise = Person.create! :first_name => "Lise", :last_name => "Meitner"   
+  end
+#START:create_people
+
   describe "GET index" do
     it "renders" do
       get :index        #<label id="code.get_index"/>
       response.should render_template(:index) #<label id="code.should_render"/>
     end
+
+#START:setup
+    it "sets up the whole list of people" do
+      get :index 
+      assigns[:people].should == [@emmy, @marie, @lise]
+    end
+#END:setup
   end
 end
 
