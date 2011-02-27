b39bc12552769d13b1840d4194c2a85fb495bb16
Sarah Allen <sarah@ultrasaurus.com>
2011-02-27 13:30:49 -0800

    removed extraneous cruft

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -9,12 +9,8 @@ describe PeopleController do
       response.should render_template(:index) #<label id="code.should_render"/>
     end
 # END:one     
-    it "renders with text param" do
-      sample_text = "something"      #<label id="code.local.sample_text"/>
-      get :index, {:text => sample_text}    #<label id="code.get.text.param"/>
-      assigns[:message].should == sample_text   #<label id="code.assigs.message"/>
-      response.should render_template(:index)
-    end
+
+   
 # START:one     
   end
 end
