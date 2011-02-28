92f60e44edb42782a015254d22bf7c69d3a3cba5
Sarah Allen <sarah@ultrasaurus.com>
2011-02-27 16:39:21 -0800

    moved :get index into before block

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -1,26 +1,24 @@
 require 'spec_helper'
 
 describe PeopleController do
-#START:create_people
   before do
     @emmy = Person.create! :first_name => "Emmy", :last_name => "Noether"
     @marie = Person.create! :first_name => "Marie", :last_name => "Curie"
     @lise = Person.create! :first_name => "Lise", :last_name => "Meitner"   
   end
-#START:create_people
 
-  describe "GET index" do
+  describe "GET index" do 
+    before do
+      get :index
+    end
+
     it "renders" do
-      get :index       
       response.should render_template(:index) 
     end
 
-#START:setup
     it "sets up the whole list of people" do
-      get :index 
       assigns[:people].should == [@emmy, @marie, @lise]
     end
-#END:setup
   end
 end
 
