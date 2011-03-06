674ab9ada775e43413501dcc294a3c3e2818f7bc
Sarah Allen <sarah@ultrasaurus.com>
2011-03-05 20:34:59 -0800

    complete RESTful people controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 ClassApp::Application.routes.draw do
 	get "people" => "people#index"  
 	post "people" => "people#create"  
+  get "people/:id" => "people#show", :as => 'person'
+  put "people/:id" => "people#update", :as => 'person'
+  delete "people/:id" => "people#destroy", :as => 'person'
+  get "people/new" => "people#new"
+  get "people/:id/edit" => "people#edit"
 end
